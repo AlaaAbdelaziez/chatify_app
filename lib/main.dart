16cfc8a1866d9438d76ae14c4e1e5350af799f95
AlaaAbdelaziez <alaaabdelaziez082@gmail.com>
2025-07-19 03:07:34 +0300

    splash_page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,34 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 //pages
 import './pages/splash_page.dart';
 
+//Services
+import './services/navigation_service.dart';
+
 void main() {
-  runApp(SplashPage(key: UniqueKey(), onInitializationComplete: () {}));
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () {
+        runApp(MainApp());
+      },
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chatify',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color.fromARGB(255, 253, 245, 230),
+        ),
+      ),
+      // navigatorKey: NavigationService.navigatorKey,
+    );
+  }
 }
