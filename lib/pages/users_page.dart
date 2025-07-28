d5da6f9c084a31fbaca22f0267772368da92afa0
AlaaAbdelaziez <alaaabdelaziez082@gmail.com>
2025-07-28 03:13:05 +0300

    imports

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//Providers
+import '../providers/authentication_provider.dart';
+
+//Widgets
+import '../widgets/top_bar.dart';
+import '../widgets/custom_input_filed.dart';
+import '../widgets/custom_list_view_tiles.dart';
+import '../widgets/rounded_button.dart';
+
+//Models
+import '../models/chat_user.dart';
+
 class UsersPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
