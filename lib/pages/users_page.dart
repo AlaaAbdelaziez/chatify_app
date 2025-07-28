61aaec6f75f5bbe177e84fbce78ac4aef26a6c85
AlaaAbdelaziez <alaaabdelaziez082@gmail.com>
2025-07-28 03:31:25 +0300

    some variables

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -23,8 +23,29 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+
+  final TextEditingController _searchFieldTextEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.greenAccent);
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight,
+      width: _deviceWidth * 0.97,
+    );
   }
 }
